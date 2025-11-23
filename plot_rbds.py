0a545032ac5a06256e82a61c7a10230e883a32e6
David Fang <david.fang@ikmail.com>
2025-11-23 02:46:03 +0100

    [Plot] video generation

--- plot_rbds.py
+++ plot_rbds.py
@@ -8,28 +8,45 @@ Options:
     --output=<dir>  Output directory [default: ./frames]
 
 """
+# Pour vidéo, besoin de faire
+# conda install -c conda-forge ffmpeg
+# conda install ffmpeg-python
+# si ça marche toujours pas
+# conda update ffmpeg
 
-
+import ffmpeg
+from dedalus.extras import plot_tools
+import matplotlib.pyplot as plt
 import h5py
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from dedalus.extras import plot_tools
+
+# Pour les colorbars :
+clims = {"u": "auto",
+         "th": [-1, 1],
+         "c": [-1, 1],
+         "vorticity": "auto"}
+tasks = ['th', 'c', 'vorticity']
+
+tasks_titles = {"u": "Velocity",
+                "th": "Temperature",
+                "c": "Salinity",
+                "vorticity": "Vorticity"}
 
 
 def main(filename, start, count, output):
     """Save plot of specified tasks for given range of analysis writes."""
 
     # Plot settings
-    tasks = ['temperature', 'salinity', 'vorticity']
+
     scale = 1.5
     dpi = 200
-    title_func = lambda sim_time: 't = {:.3f}'.format(sim_time)
-    savename_func = lambda write: 'write_{:06}.png'.format(write)
+    def title_func(sim_time): return 't = {:.3f}'.format(sim_time)
+    def savename_func(write): return 'write_{:06}.png'.format(write)
 
     # Layout
-    nrows, ncols = 2, 1
+    nrows, ncols = len(tasks), 1
     image = plot_tools.Box(4, 1)
     pad = plot_tools.Frame(0.3, 0, 0, 0)
     margin = plot_tools.Frame(0.2, 0.1, 0, 0)
@@ -47,10 +64,13 @@ def main(filename, start, count, output):
                 axes = mfig.add_axes(i, j, [0, 0, 1, 1])
                 # Call 3D plotting helper, slicing in time
                 dset = file['tasks'][task]
-                if task=='salinity':
-                    plot_tools.plot_bot_3d(dset, 0, index, axes=axes, title=task, even_scale=True, visible_axes=False, clim=(1-1e-2,1+1e-2))
+                if task not in clims or clims[task] == "auto":
+                    plot_tools.plot_bot_3d(
+                        dset, 0, index, axes=axes, title=tasks_titles[task], even_scale=True, visible_axes=False)
                 else:
-                    plot_tools.plot_bot_3d(dset, 0, index, axes=axes, title=task, even_scale=True, visible_axes=False)
+                    plot_tools.plot_bot_3d(
+                        dset, 0, index, axes=axes, title=tasks_titles[task], even_scale=True, visible_axes=False, clim=clims[task])
+
             # Add time title
             title = title_func(file['scales/sim_time'][index])
             title_height = 1 - 0.5 * mfig.margin.top / mfig.fig.y
@@ -67,9 +87,11 @@ if __name__ == "__main__":
 
     import pathlib
     from docopt import docopt
-    from dedalus.tools import logging
+    import logging
     from dedalus.tools import post
     from dedalus.tools.parallel import Sync
+
+    logger = logging.getLogger(__name__)
 
     args = docopt(__doc__)
 
@@ -79,7 +101,33 @@ if __name__ == "__main__":
         if sync.comm.rank == 0:
             if not output_path.exists():
                 output_path.mkdir()
+            else:
+                logger.info(
+                    f'{output_path} already exists. Will be overwritten.')
+    logger.info(f'Entering {output_path}')
     post.visit_writes(args['<files>'], main, output=output_path)
 
+    with Sync() as sync:
+        if sync.comm.rank == 0:
+            pattern_png = str(output_path.joinpath('write_*.png'))
 
-#     or mpiexec -n 4 python3 plot_rbds.py snapshots/2111/*.h5 --output="./frames/2111"
+            # create ./videos directory if not there
+            mp4_folder = pathlib.Path(f'./videos').absolute()
+            if not mp4_folder.exists():
+                mp4_folder.mkdir()
+            folder_name = args['<files>'][0].split("/")[-2]
+            filemp4 = str(mp4_folder.joinpath(f"{folder_name}.mp4"))
+            logger.info(f"Rendering {filemp4} from {pattern_png}")
+            fps = 60
+            ffmpeg.input(pattern_png,
+                         pattern_type='glob',
+                         framerate=fps).output(filemp4,
+                                               vcodec='libx264',
+                                               crf=18,
+                                               preset='medium',
+                                               r=fps,
+                                               pix_fmt='yuv420p',
+                                               movflags='faststart').overwrite_output().run()
+
+
+#  mpiexec -n 4 python3 plot_rbds.py snapshots/2111/*.h5 --output="./frames/2111"

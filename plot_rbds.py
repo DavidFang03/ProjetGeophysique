d925eb0f89836080f659382e102678c3280cf50f
David Fang <david.fang@ikmail.com>
2025-12-06 16:01:41 +0100

    more plots, erase existing frames and use run_name as path

--- plot_rbds.py
+++ plot_rbds.py
@@ -2,10 +2,7 @@
 Plot 2D cartesian snapshots.
 
 Usage:
-    plot_rbds.py <files>... [--output=<dir>]
-
-Options:
-    --output=<dir>  Output directory [default: ./frames]
+    plot_rbds.py
 
 """
 
@@ -15,7 +12,6 @@ Options:
 # si ça marche toujours pas
 # conda update ffmpeg
 
-import ffmpeg
 from dedalus.extras import plot_tools
 import matplotlib.pyplot as plt
 import h5py
@@ -24,17 +20,49 @@ import matplotlib
 
 matplotlib.use("Agg")
 
-######################################################
+# mpiexec -n 8 python3 plot_rbds.py
+## !####################################################
+run_name = "0612_Ra1e+04_Flot1e-01_X100_Y100_Le10_Pr1"
+make_movie = True
+clean_replot = True  # S'il y a deja des frames, repartir de zero.
 all_tasks = {
     "th": {"title": "Temperature", "type": "3d", "clim": [0, 1]},
     "c": {"title": "Salinity", "type": "3d", "clim": "auto"},
     "vorticity": {"title": "Vorticity", "type": "3d", "clim": "auto"},
-    "meanx_th": {"title": "Temperature horizontal average", "type": "2d", "axis": "z"},
-    "meanx_c": {"title": "Salinity horizontal average", "type": "2d", "axis": "z"},
-    "flux_th": {"title": "Thermal flux at interface", "type": "2d", "axis": "x"},
-    "flux_c": {"title": "Salt flux at interface", "type": "2d", "axis": "x"},
+    "vz_times_theta": {"title": r"$v_z\times\theta$", "type": "3d", "clim": "auto"},
+    "vz_times_c": {"title": r"$v_z\times c$", "type": "3d", "clim": "auto"},
+    "meanx_th": {
+        "title": "Temperature horizontal average",
+        "type": "2d",
+        "axis": "z",
+        "range": [None, None],
+    },
+    "meanx_c": {
+        "title": "Salinity horizontal average",
+        "type": "2d",
+        "axis": "z",
+        "range": [None, None],
+    },
+    "flux_th": {
+        "title": "Thermal flux at interface",
+        "type": "2d",
+        "axis": "x",
+        "range": [None, None],
+    },
+    "flux_c": {
+        "title": "Salt flux at interface",
+        "type": "2d",
+        "axis": "x",
+        "range": [None, None],
+    },
+    "h": {
+        "title": "Ice layer height",
+        "type": "2d",
+        "axis": "x",
+        "range": [None, None],
+    },
 }
-######################################################
+## !####################################################
 
 
 def main(filename, start, count, output):
@@ -43,7 +71,7 @@ def main(filename, start, count, output):
     # Plot settings
 
     scale = 1.5
-    dpi = 200
+    dpi = 150
 
     def title_func(sim_time):
         return "t = {:.3f}".format(sim_time)
@@ -52,45 +80,49 @@ def main(filename, start, count, output):
         return "write_{:06}.png".format(write)
 
     # Layout
-    nrows, ncols = 3, 4  # à change si jamais + de plots
-    image = plot_tools.Box(4, 1)
+    nrows, ncols = 4, 4  # à changer si jamais + de plots
+    image = plot_tools.Box(4, 1)  # Lx=4, Lz=1
     pad = plot_tools.Frame(0.3, 0, 0, 0)
     margin = plot_tools.Frame(0.2, 0.1, 0, 0)
 
     # Create multifigure
-    mfig = plot_tools.MultiFigure(nrows, ncols, image, pad, margin, scale)
+    mfig = plot_tools.MultiFigure(nrows, ncols + 1, image, pad, margin, scale)
     fig = mfig.figure
 
     # Plot writes
     with h5py.File(filename, mode="r") as file:
         for index in range(start, start + count):
-            filled_rows = [0, 0, 0]
+            filled_rows = [
+                0,
+                0,
+                0,
+                0,
+            ]  # Deux premières colonnes pour les plots 3d (colorbar) et les deux autres pour les 2d (profil spatial)
+            row3d = 0  # passera à 1 si la 1ère colonne pleine
             for n, (task, task_infos) in enumerate(all_tasks.items()):
                 # Build subfigure axes
                 plot_type = task_infos["type"]
 
                 if plot_type == "3d":
-                    filled_rows[0] += 1
-                    i = filled_rows[0] - 1
-                    j = 0
+                    if filled_rows[row3d] == 3:
+                        row3d += 1
+                    j = row3d
                 elif plot_type == "2d":
                     axis = task_infos["axis"]
                     if axis == "x":
-                        filled_rows[1] += 1
-                        i = filled_rows[1] - 1
-                        j = 1
-                    elif axis == "z":
-                        filled_rows[2] += 1
-                        i = filled_rows[2] - 1
                         j = 2
+                    elif axis == "z":
+                        j = 3
+                filled_rows[j] += 1
+                i = filled_rows[j] - 1
+
                 # i, j = divmod(n, ncols)
 
                 # Call 3D plotting helper, slicing in time
                 dset = file["tasks"][task]
-                print(dset.shape)
                 task_title = task_infos["title"]
                 if task_infos["type"] == "3d":
-                    axes = mfig.add_axes(i, j, [0, 0, 1, 1])
+                    axes = mfig.add_axes(i, j, [j / 4, 0, 1, 1])
                     if "clim" not in task_infos or task_infos["clim"] == "auto":
                         plot_tools.plot_bot_3d(
                             dset,
@@ -114,21 +146,24 @@ def main(filename, start, count, output):
 
                     x = dset.dims[1][0][:].ravel()
                     z = dset.dims[2][0][:].ravel()
+                    values_min, values_max = task_infos["range"]
                     if task_infos["axis"] == "x":
-                        axes2d = mfig.add_axes(i, j, [0.33, 0, 1, 1])
+                        axes2d = mfig.add_axes(i, j, [j / 4, 0, 1, 1])
                         data = dset[index, :, 0]
                         axes2d.plot(x, data)
                         axes2d.set_xlabel("x")
                         axes2d.set_ylabel(task_title)
                         axes2d.set_xticks([])
+                        axes2d.set_ylim(values_min, values_max, auto=True)
 
                     elif task_infos["axis"] == "z":
-                        axes2d = mfig.add_axes(i, j, [0.66, 0, 1, 1])
+                        axes2d = mfig.add_axes(i, j, [j / 4, 0, 1, 1])
                         data = dset[index, 0, :]
                         axes2d.plot(data, z)
                         axes2d.set_xlabel(task_title)
                         axes2d.set_ylabel("z")
                         axes2d.set_yticks([])
+                        axes2d.set_xlim(values_min, values_max, auto=True)
 
                     # axes2d.title.set_text(task_title)
 
@@ -138,7 +173,7 @@ def main(filename, start, count, output):
             # Add time title
             title = title_func(file["scales/sim_time"][index])
             title_height = 1 - 0.5 * mfig.margin.top / mfig.fig.y
-            fig.suptitle(title, x=0.44, y=title_height, ha="left")
+            fig.suptitle(f"{run_name} \n {title}", y=title_height, ha="center")
             # Save figure
             savename = savename_func(file["scales/write_number"][index])
             savepath = output.joinpath(savename)
@@ -149,16 +184,25 @@ def main(filename, start, count, output):
 
 
 if __name__ == "__main__":
-
     import pathlib
-    from docopt import docopt
+
+    # from docopt import docopt
     import logging
     from dedalus.tools import post
     from dedalus.tools.parallel import Sync
+    import glob
 
     logger = logging.getLogger(__name__)
 
-    args = docopt(__doc__)
+    # args = docopt(__doc__)
+
+    files = f"snapshots/{run_name}/*.h5"
+    outputs = f"frames/{run_name}"
+    args = {"--output": outputs, "<files>": files}
+    globlist = glob.glob(args["<files>"])
+
+    with h5py.File(globlist[0], mode="r") as file:
+        logger.info("Available data:", file["tasks"].keys())
 
     output_path = pathlib.Path(args["--output"]).absolute()
     # Create output directory if needed
@@ -168,30 +212,51 @@ if __name__ == "__main__":
                 output_path.mkdir()
             else:
                 logger.info(f"{output_path} already exists. Will be overwritten.")
+                previousframes_pattern = f"{output_path}/*.png"
+                previousframes = glob.glob(previousframes_pattern)
+
+                if clean_replot and len(previousframes) > 0:
+                    user_agree = (
+                        input(
+                            f"Will remove {len(previousframes)} frames: {previousframes_pattern} (y/n)"
+                        )
+                        == "y"
+                    )
+                    if user_agree:
+                        import os
+
+                        for f in previousframes:
+                            os.remove(f)
+
     logger.info(f"Entering {output_path}")
-    post.visit_writes(args["<files>"], main, output=output_path)
+    post.visit_writes(globlist, main, output=output_path)
+
+    if make_movie:
+        import ffmpeg
+
+        with Sync() as sync:
+            if sync.comm.rank == 0:
+                pattern_png = str(output_path.joinpath("write_*.png"))
+
+                # create ./videos directory if not there
+                mp4_folder = pathlib.Path(f"./videos").absolute()
+                if not mp4_folder.exists():
+                    mp4_folder.mkdir()
+                # folder_name = args["<files>"][0].split("/")[-2]
+                filemp4 = str(mp4_folder.joinpath(f"{run_name}.mp4"))
+                logger.info(f"Rendering {filemp4} from {pattern_png}")
+                fps = 60
+                ffmpeg.input(pattern_png, pattern_type="glob", framerate=fps).output(
+                    filemp4,
+                    vcodec="libx264",
+                    crf=18,
+                    preset="medium",
+                    r=fps,
+                    pix_fmt="yuv420p",
+                    movflags="faststart",
+                ).overwrite_output().run()
 
     with Sync() as sync:
         if sync.comm.rank == 0:
-            pattern_png = str(output_path.joinpath("write_*.png"))
-
-            # create ./videos directory if not there
-            mp4_folder = pathlib.Path(f"./videos").absolute()
-            if not mp4_folder.exists():
-                mp4_folder.mkdir()
-            folder_name = args["<files>"][0].split("/")[-2]
-            filemp4 = str(mp4_folder.joinpath(f"{folder_name}.mp4"))
-            logger.info(f"Rendering {filemp4} from {pattern_png}")
-            fps = 60
-            ffmpeg.input(pattern_png, pattern_type="glob", framerate=fps).output(
-                filemp4,
-                vcodec="libx264",
-                crf=18,
-                preset="medium",
-                r=fps,
-                pix_fmt="yuv420p",
-                movflags="faststart",
-            ).overwrite_output().run()
-
-
-#  mpiexec -n 8 python3 plot_rbds.py snapshots/0512/*.h5 --output="./frames/0512"
+            frames_pattern = f"{output_path}/*.png"
+            logger.info(f"{len(frames_pattern)} frames have been written.")
